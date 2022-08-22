be38aaa7c5d4ca75d48ae5216bdcf3eca1c7a6dc
Om <88827091+mobileappdev47@users.noreply.github.com>
2022-08-22 18:21:10 +0530

    add icons

---
+++ lib/screen/profile/profile.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 50,
+              width: Get.width,
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/utils/image_res.dart
+++ lib/utils/image_res.dart
@@ -5,4 +5,16 @@ class ImageRes {
   static String sliderPage3 = "${imagePath}slider_page3.png";
   static String loginScreenTop = "${imagePath}login_screen_top.png";
   static String appLogo = "${imagePath}app_logo.png";
+  static String menu_icon = "${imagePath}menu_icon.png";
+  static String payroll = "${imagePath}payroll.png";
+  static String attendance = "${imagePath}attendence.png";
+  static String notice = "${imagePath}notice.png";
+  static String expenses = "${imagePath}expenses.png";
+  // static String leave = "${imagePath}Leave.png";
+  static String approval = "${imagePath}approval.png";
+  static String visit = "${imagePath}visit.png";
+  static String appoinment = "${imagePath}appointment.png";
+  static String break_icon = "${imagePath}break.png";
+  static String daily_leave = "${imagePath}daily_leave.png";
+
 }
