44f944fa951ac8136336587926f3b645f63984b3
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2022-08-27 12:36:33 +0530

    add text field

--- lib/screen/dashboard/pms_edit_screen/pms_edit_screen_controller.dart
+++ lib/screen/dashboard/pms_edit_screen/pms_edit_screen_controller.dart
@@ -1,22 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PmsEditScreenController extends GetxController implements GetxService {
   List text = ["Shared Goals", "Finance", "Customer", "Process", "Best People"];
   RxList selected = [].obs;
   RxBool isShow = false.obs;
+  TextEditingController selfReviewController = TextEditingController();
 
   onChangeShow() {
-
-    if (isShow.value==true) {
+    if (isShow.value == true) {
       isShow.value = false;
     } else {
       isShow.value = true;
     }
   }
-  onAddRemoveText(String text){
-    if(selected.contains(text)){
+
+  onAddRemoveText(String text) {
+    if (selected.contains(text)) {
       selected.remove(text);
-    }else{
+    } else {
       selected.add(text);
     }
   }
